23d4f26cce444a019bf0f4668da4fd351604f241
Pavel-Kr <57057057+Pavel-Kr@users.noreply.github.com>
2022-10-25 22:11:36 +0700

    Add files via upload

--- lab3.c
+++ lab3.c
@@ -9,6 +9,9 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <math.h>
+
+//#define LOGGING
+
 unsigned int block(char *arg)
 {
     int i = 0;
@@ -29,17 +32,22 @@ unsigned int block(char *arg)
 }
 void test_RAM(unsigned int block_size, double *read_result, double *write_result)
 {
-    //int log=open("log.txt",O_WRONLY|O_CREAT|O_APPEND,S_IRWXU);
-    char *block = malloc(block_size);
-    char *buf = malloc(block_size);
+#ifdef LOGGING
+    int log = open("log.txt", O_WRONLY | O_CREAT | O_APPEND, S_IRWXU);
+    char log_message[100];
+#endif
+    unsigned int buf_size=block_size/sizeof(int);
+    int *block = malloc(buf_size*sizeof(int));
+    int *buf = malloc(buf_size*sizeof(int));
     struct timespec start, finish;
     double sec = 0, nsec = 0;
-    for (int i = 0; i < block_size; i++)
+    *write_result = 0;
+    for (int i = 0; i < buf_size; i++)
     {
-        buf[i] = rand() % 256;
+        buf[i] = rand();
     }
     clock_gettime(CLOCK_REALTIME, &start);
-    for (int i = 0; i < block_size; i++)
+    for (int i = 0; i < buf_size; i++)
     {
         block[i] = buf[i];
     }
@@ -52,15 +60,19 @@ void test_RAM(unsigned int block_size, double *read_result, double *write_result
         nsec += 1E9;
     }
     *write_result = sec + nsec / 1E9;
-    // char log_message[100];
-    // sprintf(log_message,"Write: %ld - lsec, %f - dsec, %ld - lnsec, %f - dnsec, %f - results\n",finish.tv_sec - start.tv_sec,sec,finish.tv_nsec-start.tv_nsec,nsec,*write_result);
-    // write(log,log_message,strlen(log_message));
+#ifdef LOGGING
+    sprintf(log_message, "Write: %ld - start.sec, %ld - start.nsec, %ld - finish.sec, %ld - finish.nsec\n", start.tv_sec, start.tv_nsec, finish.tv_sec, finish.tv_nsec);
+    write(log, log_message, strlen(log_message));
+    sprintf(log_message, "\t%ld - dif.sec, %ld - dif.nsec\n", finish.tv_sec - start.tv_sec, finish.tv_nsec - start.tv_nsec);
+    write(log, log_message, strlen(log_message));
+    sprintf(log_message, "\t%f - dsec, %f - dnsec, %f - result\n", sec, nsec, *write_result);
+    write(log, log_message, strlen(log_message));
+#endif
     sec = nsec = 0;
+    *read_result = 0;
     clock_gettime(CLOCK_REALTIME, &start);
-    char tmp;
-    for (unsigned int i = 0; i < block_size; i++)
+    for (unsigned int i = 0; i < buf_size; i++)
     {
-        //tmp = 
         block[i];
     }
     clock_gettime(CLOCK_REALTIME, &finish);
@@ -71,25 +83,36 @@ void test_RAM(unsigned int block_size, double *read_result, double *write_result
         sec--;
         nsec += 1E9;
     }
-    *read_result = sec + nsec / 1E9;
-    // sprintf(log_message,"Read: %ld - lsec, %f - dsec, %ld - lnsec, %f - dnsec, %f - results\n",finish.tv_sec - start.tv_sec,sec,finish.tv_nsec-start.tv_nsec,nsec,*read_result);
-    // write(log,log_message,strlen(log_message));
+    *read_result += sec + nsec / 1E9;
+#ifdef LOGGING
+    sprintf(log_message, "Read: %ld - start.sec, %ld - start.nsec, %ld - finish.sec, %ld - finish.nsec\n", start.tv_sec, start.tv_nsec, finish.tv_sec, finish.tv_nsec);
+    write(log, log_message, strlen(log_message));
+    sprintf(log_message, "\t%ld - dif.sec, %ld - dif.nsec\n", finish.tv_sec - start.tv_sec, finish.tv_nsec - start.tv_nsec);
+    write(log, log_message, strlen(log_message));
+    sprintf(log_message, "\t%f - dsec, %f - dnsec, %f - result\n", sec, nsec, *read_result);
+    write(log, log_message, strlen(log_message));
+#endif
     free(buf);
     free(block);
-    //close(log);
+#ifdef LOGGING
+    close(log);
+#endif
 }
 void test_SSD(int fd, unsigned int block_size, double *read_result, double *write_result)
 {
-    //int log=open("log.txt",O_WRONLY|O_CREAT|O_APPEND,S_IRWXU);
-    char *block = malloc(block_size);
+#ifdef LOGGING
+    int log = open("log.txt", O_WRONLY | O_CREAT | O_APPEND, S_IRWXU);
+#endif
+    unsigned int buf_size=block_size/sizeof(int);
+    int *block = malloc(buf_size*sizeof(int));
     struct timespec start, finish;
     double sec = 0, nsec = 0;
-    for (int i = 0; i < block_size; i++)
+    for (int i = 0; i < buf_size; i++)
     {
-        block[i] = rand() % 256;
+        block[i] = rand();
     }
     clock_gettime(CLOCK_REALTIME, &start);
-    write(fd, block, block_size);
+    write(fd, block, buf_size);
     fsync(fd);
     clock_gettime(CLOCK_REALTIME, &finish);
     sec = finish.tv_sec - start.tv_sec;
@@ -100,18 +123,20 @@ void test_SSD(int fd, unsigned int block_size, double *read_result, double *writ
         nsec += 1E9;
     }
     *write_result = sec + nsec / 1E9;
-    // char log_message[100];
-    // sprintf(log_message,"Write: %ld - start.sec, %ld - start.nsec, %ld - finish.sec, %ld - finish.nsec\n",start.tv_sec,start.tv_nsec,finish.tv_sec,finish.tv_nsec);
-    // write(log,log_message,strlen(log_message));
-    // sprintf(log_message,"\t%ld - dif.sec, %ld - dif.nsec\n",finish.tv_sec-start.tv_sec,finish.tv_nsec-start.tv_nsec);
-    // write(log,log_message,strlen(log_message));
-    // sprintf(log_message,"\t%f - dsec, %f - dnsec, %f - result\n",sec,nsec,*write_result);
-    // write(log,log_message,strlen(log_message));
+#ifdef LOGGING
+    char log_message[100];
+    sprintf(log_message, "Write: %ld - start.sec, %ld - start.nsec, %ld - finish.sec, %ld - finish.nsec\n", start.tv_sec, start.tv_nsec, finish.tv_sec, finish.tv_nsec);
+    write(log, log_message, strlen(log_message));
+    sprintf(log_message, "\t%ld - dif.sec, %ld - dif.nsec\n", finish.tv_sec - start.tv_sec, finish.tv_nsec - start.tv_nsec);
+    write(log, log_message, strlen(log_message));
+    sprintf(log_message, "\t%f - dsec, %f - dnsec, %f - result\n", sec, nsec, *write_result);
+    write(log, log_message, strlen(log_message));
+#endif
     sec = nsec = 0;
-    memset(block,0,block_size);
-    lseek(fd,0,SEEK_SET);
+    memset(block, 0, buf_size);
+    lseek(fd, 0, SEEK_SET);
     clock_gettime(CLOCK_REALTIME, &start);
-    read(fd, block, block_size);
+    read(fd, block, buf_size);
     clock_gettime(CLOCK_REALTIME, &finish);
     sec = finish.tv_sec - start.tv_sec;
     nsec = finish.tv_nsec - start.tv_nsec;
@@ -121,20 +146,22 @@ void test_SSD(int fd, unsigned int block_size, double *read_result, double *writ
         nsec += 1E9;
     }
     *read_result = sec + nsec / 1E9;
-    // sprintf(log_message,"Read: %ld - start.sec, %ld - start.nsec, %ld - finish.sec, %ld - finish.nsec\n",start.tv_sec,start.tv_nsec,finish.tv_sec,finish.tv_nsec);
-    // write(log,log_message,strlen(log_message));
-    // sprintf(log_message,"\t%ld - dif.sec, %ld - dif.nsec\n",finish.tv_sec-start.tv_sec,finish.tv_nsec-start.tv_nsec);
-    // write(log,log_message,strlen(log_message));
-    // sprintf(log_message,"\t%f - dsec, %f - dnsec, %f - result\n",sec,nsec,*read_result);
-    // write(log,log_message,strlen(log_message));
+#ifdef LOGGING
+    sprintf(log_message, "Read: %ld - start.sec, %ld - start.nsec, %ld - finish.sec, %ld - finish.nsec\n", start.tv_sec, start.tv_nsec, finish.tv_sec, finish.tv_nsec);
+    write(log, log_message, strlen(log_message));
+    sprintf(log_message, "\t%ld - dif.sec, %ld - dif.nsec\n", finish.tv_sec - start.tv_sec, finish.tv_nsec - start.tv_nsec);
+    write(log, log_message, strlen(log_message));
+    sprintf(log_message, "\t%f - dsec, %f - dnsec, %f - result\n", sec, nsec, *read_result);
+    write(log, log_message, strlen(log_message));
+    close(log);
+#endif
     free(block);
-    //close(log);
 }
 void report(FILE *csv, char *memory_type, unsigned int block_size, int launch_num, double write_time, double average_write, double read_time, double average_read)
 {
     fprintf(csv, "%s;", memory_type);
     fprintf(csv, "%d;", block_size);
-    fprintf(csv, "char;");
+    fprintf(csv, "int;");
     fprintf(csv, "%d;", block_size);
     fprintf(csv, "%d;", launch_num);
     fprintf(csv, "clock_gettime;");
@@ -182,7 +209,7 @@ int main(int argc, char **argv)
     double average_read = 0, average_write = 0;
     for (int i = 0; i < launch_count; i++)
     {
-        printf("Testing %s with %d bytes of memory: %d/%d\n",memory_type,block_size,i+1,launch_count);
+        printf("Testing %s with %d bytes of memory: %d/%d\n", memory_type, block_size, i + 1, launch_count);
         if (strcmp(memory_type, "RAM") == 0)
             test_RAM(block_size, &read_results[i], &write_results[i]);
         else if (strcmp(memory_type, "SSD") == 0)
@@ -191,7 +218,8 @@ int main(int argc, char **argv)
             test_SSD(fd, block_size, &read_results[i], &write_results[i]);
             unlink("test_file.dat");
         }
-        else if(strcmp(memory_type, "flash") == 0){
+        else if (strcmp(memory_type, "flash") == 0)
+        {
             int fd = open("/media/sofya/TRANSCEND/test_file.dat", O_RDWR | O_CREAT, S_IRWXU);
             test_SSD(fd, block_size, &read_results[i], &write_results[i]);
             unlink("/media/sofya/TRANSCEND/test_file.dat");
@@ -199,7 +227,6 @@ int main(int argc, char **argv)
         printf("\033c");
         average_read += read_results[i];
         average_write += write_results[i];
-    
     }
     average_write /= launch_count;
     average_read /= launch_count;
